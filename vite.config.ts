d795b1422631f26e13593437fdb6855823fb96d9
yuneco <yukiyuneco@gmail.com>
2023-12-17 18:28:04 +0900

    config for pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
 import KumaUI from "@kuma-ui/vite";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), KumaUI()],
+  base: "/awesome-bounding-box/",
 });
